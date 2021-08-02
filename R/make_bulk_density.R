a23d6f4b2b99d42e50b7105850d89ad9442ca3a7
K Todd-Brown <ktoddbrown@ufl.edu>
2021-08-02 18:36:45 -0400

    spin out the data source and the bulk density  sematnics into its own function

---
+++ R/make_bulk_density.R
@@ -0,0 +1,36 @@
+#' Make semantic framework for bulk density
+#' 
+#' A soil bulk density value generally uses the methods specified in the list structure defined here.
+#' Note that coarse fraction is generally needed to interpret the bulk density measurement and also defined here.
+#'
+#' @return a list describing the information needed to utilize a bulk density measurement
+#' @export
+#'
+#' @examples
+#' bd.ls <- make_bulk_density()
+#' 
+make_bulk_density <- function(){
+  bulk_density.ls <- list(bulk_density_value = list(description = 'Bulk density - dry soil to volume ratio, specify inclusion of coarse fraction in data', 
+                                                    type = 'numeric', min = 0, max = Inf),                        
+                          bulk_density_units = list(description= 'Bulk density unit [mass per volume]', 
+                                                    type = 'character'),
+                          bulk_density_volume_method = list(description = 'Bulk density volume method', 
+                                                            type = list(geometric_volume = 'Volume by extracted slice or core geometry.',
+                                                                           compliant_cavity = 'Fill hole with material of known density',
+                                                                           clod = 'Volume by displacement of soil mass.',
+                                                                           auger_cores = 'Internal core volume of an auger core',
+                                                                           imaged_volume = '3D scan for volume of soil clod')),
+                          bulk_density_includes_coarse_fraction = list(description = 'Flags inclusion of coarse fraction in bulk density value',
+                                                                       type = 'logical'),
+                          coarse_fraction_value = list(description = 'Coarse fraction', 
+                                                       type = 'numeric', min = 0, max = 1),
+                          coarse_fraction_unit = list(description = 'Coarse fraction unit', 
+                                                      type = list(mass_ratio = 'ratio of coarse fraction mass to whole soil mass [common]', 
+                                                                     volume_ratio = 'ratio of coarse fraction volume to whole soil volume [rare]')),
+                          coarse_fraction_method = list(description = 'Coarse fraction separation methods', 
+                                                        type = list(sieve_2mm = ' >2mm sieve size [common]', 
+                                                                       hand_picked = 'hand picked large partices (ex roots, and rocks), generally peats [rare]'))
+  )
+  
+  return(bulk_density.ls)
+}
---
+++ R/make_data_source.R
@@ -0,0 +1,35 @@
+#' Make semantic framework for data source
+#'
+#'What information is generally needed to download and access a new data source
+#'
+#' @return a list with the key parts of information and defined types
+#' @export
+#'
+#' @examples
+#' source.ls <- make_data_source()
+make_data_source <- function(){
+  data_source.ls <- list(data_source_ID = list(desciption = 'Internal study identifier; convention [FirstAuthorLastName-GroupName][Year][abc] ex Yamshito2015a',
+                                               type = 'character'), #TODO formatting check here?
+                         data_source_title = list(desciption = 'Long title of data source',
+                                                  type = 'character'),
+                         data_source_access = list(description = 'how the data was accessed',
+                                                   type = list(data_package = 'download as a data package zip file including metadata and data tables, could include paper as well',
+                                                                 data_table_files = 'download as individual data tables with one header row regular structure. Possible meta information in header defined as /* */ from pangaea',
+                                                                 data_rescue = 'manual or scripted extraction needed from a scanned document or non-regular table'
+                                                   )),
+                         data_source_download_URL = list(description = 'list of for data download, as ; seperated list',
+                                                         type = 'character'), #TODO formatting check here?
+                         data_source_filename = list(description = 'list of data file names that match the data downloads',
+                                                     type = 'character'),
+                         data_source_citation = list(description = 'suggested data citation',
+                                                     type = 'character'),
+                         data_source_DOI = list(description = 'DOI for associated data object, might be different from download URL',
+                                                type = 'character'),
+                         data_source_published_year = list(description = 'Year data was published [Common Era]',
+                                                           type = 'numeric', max = 'Inf', min = 0),
+                         data_source_abstract = list(description = 'Abstract for data citation',
+                                                     type = 'character'),
+                         data_source_notes = list(description = 'Free notes field: why this data source was selected,list of data variables, keywords, and associated publications',
+                                                  type = 'character'))
+  return(data_source.ls)
+}
