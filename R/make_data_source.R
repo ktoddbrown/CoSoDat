25277a2f9ade135cb7d238e27c696f087aef5060
K Todd-Brown <ktoddbrown@ufl.edu>
2021-08-04 08:50:40 -0400

    Add notes about possible redundent fields; streamlined data source access factors

--- R/make_data_source.R
+++ R/make_data_source.R
@@ -10,12 +10,11 @@
 make_data_source <- function(){
   data_source.ls <- list(data_source_ID = list(desciption = 'Internal study identifier; convention [FirstAuthorLastName-GroupName][Year][abc] ex Yamshito2015a',
                                                type = 'character'), #TODO formatting check here?
-                         data_source_title = list(desciption = 'Long title of data source',
+                         data_source_title = list(desciption = 'Long title of data source', #possibly redundent given citation
                                                   type = 'character'),
                          data_source_access = list(description = 'how the data was accessed',
-                                                   type = list(data_package = 'download as a data package zip file including metadata and data tables, could include paper as well',
-                                                                 data_table_files = 'download as individual data tables with one header row regular structure. Possible meta information in header defined as /* */ from pangaea',
-                                                                 data_rescue = 'manual or scripted extraction needed from a scanned document or non-regular table'
+                                                   type = list(regular_tables = 'download as individual data tables or as zipfiles with one header row and regular structure. Possible meta information in header defined as /* */ from Pangaea or as xml-style file from EDI.',
+                                                               rescue = 'manual or scripted extraction needed from a scanned document or non-regular table'
                                                    )),
                          data_source_download_URL = list(description = 'list of for data download, as ; seperated list',
                                                          type = 'character'), #TODO formatting check here?
@@ -26,7 +25,7 @@ make_data_source <- function(){
                          data_source_DOI = list(description = 'DOI for associated data object, might be different from download URL',
                                                 type = 'character'),
                          data_source_published_year = list(description = 'Year data was published [Common Era]',
-                                                           type = 'numeric', max = 'Inf', min = 0),
+                                                           type = 'numeric', max = 'Inf', min = 0), # possibly redundant given citation
                          data_source_abstract = list(description = 'Abstract for data citation',
                                                      type = 'character'),
                          data_source_notes = list(description = 'Free notes field: why this data source was selected,list of data variables, keywords, and associated publications',
