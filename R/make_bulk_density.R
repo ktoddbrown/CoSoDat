962551071c601aa7507276c83a71a42457ee692c
K Todd-Brown <ktoddbrown@ufl.edu>
2021-08-05 10:54:03 -0400

    Stream line bulk density description using value-unit-method triple structure

--- R/make_bulk_density.R
+++ R/make_bulk_density.R
@@ -2,7 +2,7 @@
 #' 
 #' A soil bulk density value generally uses the methods specified in the list structure defined here.
 #' Note that coarse fraction is generally needed to interpret the bulk density measurement and also defined here.
-#' All masses are assumed to reflect dry weight, either measured or inferred.
+#' All masses are assumed to reflect dry weight, either measured or inferred, this may change in the future as we extend to hydrological measurements.
 #'
 #' @return a list describing the information needed to utilize a bulk density measurement
 #' @export
@@ -11,26 +11,28 @@
 #' bd.ls <- make_bulk_density()
 #' 
 make_bulk_density <- function(){
-  bulk_density.ls <- list(bulk_density_value = list(description = 'Bulk density - dry soil to volume ratio, specify inclusion of coarse fraction in data', 
-                                                    type = 'numeric', min = 0, max = Inf),                        
-                          bulk_density_units = list(description= 'Bulk density unit [mass per volume]', 
-                                                    type = 'character'),
-                          bulk_density_volume_method = list(description = 'Bulk density volume method', 
-                                                            type = list(geometric_volume = 'Volume by extracted slice or core geometry.',
-                                                                           compliant_cavity = 'Fill hole with material of known density',
-                                                                           clod = 'Volume by displacement of soil mass.',
-                                                                           auger_cores = 'Internal core volume of an auger core',
-                                                                           imaged_volume = '3D scan for volume of soil clod')),
-                          bulk_density_includes_coarse_fraction = list(description = 'Flags inclusion of coarse fraction in bulk density value',
-                                                                       type = 'logical'),
-                          coarse_fraction_value = list(description = 'Coarse fraction', 
-                                                       type = 'numeric', min = 0, max = 1),
-                          coarse_fraction_unit = list(description = 'Coarse fraction unit', 
-                                                      type = list(mass_ratio = 'ratio of coarse fraction mass to whole soil mass [common]', 
-                                                                     volume_ratio = 'ratio of coarse fraction volume to whole soil volume [rare]')),
-                          coarse_fraction_method = list(description = 'Coarse fraction separation methods', 
-                                                        type = list(sieve_2mm = ' >2mm sieve size [common]', 
-                                                                       hand_picked = 'hand picked large partices (ex roots, and rocks), generally peats [rare]'))
+  bulk_density.ls <- list(bulk_density = list(value = list(description = 'Bulk density - dry soil to volume ratio, specify inclusion of coarse fraction in data', 
+                                                           range = list(min = 0, max = Inf)),
+                                              units = list(description= 'Bulk density unit [mass per volume]', 
+                                                           control_vocabulary = list(g_per_cm3 = 'grams per centimeter cubed',
+                                                                                     kg_per_m3 = 'killograms per meter cubed')),
+                                              method = list(volume = list(description = 'Bulk density volume method', 
+                                                                          control_vocabulary = list(geometric_volume = 'Volume by extracted slice or core geometry.',
+                                                                                                    compliant_cavity = 'Fill hole with material of known density',
+                                                                                                    clod = 'Volume by displacement of soil mass.',
+                                                                                                    auger_cores = 'Internal core volume of an auger core',
+                                                                                                    imaged_volume = '3D scan for volume of soil clod')),
+                                                            mass = list(description = 'Flags inclusion of coarse fraction in bulk density value',
+                                                                        control_vocabulary = list(includes_coarse_fraction = 'Coarse fraction is include in mass',
+                                                                                                  excludes_coarse_fraction = 'Coarse fraction is excluded in mass')))),
+                          coarse_fraction = list(value = list(description = 'Coarse fraction', 
+                                                              range = list(min = 0, max = 1)),
+                                                 unit = list(description = 'Coarse fraction unit', 
+                                                             control_vocabulary = list(mass_per_mass = 'ratio of coarse fraction mass to whole soil mass [common]', 
+                                                                                       volume_per_volume = 'ratio of coarse fraction volume to whole soil volume [rare]')),
+                                                 method = list(description = 'Coarse fraction separation methods', 
+                                                               control_vocabulary = list(sieve_2mm = ' >2mm sieve size [common]', 
+                                                                                         hand_picked = 'hand picked large partices (ex roots, and rocks), generally peats [rare]')))
   )
   
   return(bulk_density.ls)
--- R/make_data_source.R
+++ R/make_data_source.R
@@ -10,24 +10,22 @@
 make_data_source <- function(){
   data_source.ls <- list(data_source_ID = list(desciption = 'Internal study identifier; convention [FirstAuthorLastName-GroupName][Year][abc] ex Yamshito2015a',
                                                type = 'character'), #TODO formatting check here?
-                         data_source_title = list(desciption = 'Long title of data source', #possibly redundent given citation
-                                                  type = 'character'),
                          data_source_access = list(description = 'how the data was accessed',
                                                    type = list(regular_tables = 'download as individual data tables or as zipfiles with one header row and regular structure. Possible meta information in header defined as /* */ from Pangaea or as xml-style file from EDI.',
-                                                               rescue = 'manual or scripted extraction needed from a scanned document or non-regular table'
+                                                               rescue = 'manual or one-off scripted extraction needed from a scanned document or non-regular table'
                                                    )),
                          data_source_download_URL = list(description = 'list of for data download, as ; seperated list',
                                                          type = 'character'), #TODO formatting check here?
-                         data_source_filename = list(description = 'list of data file names that match the data downloads',
+                         data_source_filename = list(description = 'list of data file names that match the data downloads, as ; seperated list',
                                                      type = 'character'),
-                         data_source_citation = list(description = 'suggested data citation',
+                         data_source_citation = list(description = 'suggested data citation, includes acknowledgements',
                                                      type = 'character'),
                          data_source_DOI = list(description = 'DOI for associated data object, might be different from download URL',
                                                 type = 'character'),
-                         data_source_published_year = list(description = 'Year data was published [Common Era]',
-                                                           type = 'numeric', max = 'Inf', min = 0), # possibly redundant given citation
                          data_source_abstract = list(description = 'Abstract for data citation',
                                                      type = 'character'),
+                         data_source_license = list(description = 'data license and use-case restrictions',
+                                                    type = 'chacacter'),
                          data_source_notes = list(description = 'Free notes field: why this data source was selected,list of data variables, keywords, and associated publications',
                                                   type = 'character'))
   return(data_source.ls)
