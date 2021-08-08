36d396423ef41246ea1460ac41dcb553be448e89
K Todd-Brown <ktoddbrown@ufl.edu>
2021-08-08 15:42:51 -0400

    adding wrap-up draft from first hackathon

--- R/make_data_source.R
+++ R/make_data_source.R
@@ -10,23 +10,22 @@
 make_data_source <- function(){
   data_source.ls <- list(data_source_ID = list(desciption = 'Internal study identifier; convention [FirstAuthorLastName-GroupName][Year][abc] ex Yamshito2015a',
                                                type = 'character'), #TODO formatting check here?
-                         data_source_access = list(description = 'how the data was accessed',
-                                                   type = list(regular_tables = 'download as individual data tables or as zipfiles with one header row and regular structure. Possible meta information in header defined as /* */ from Pangaea or as xml-style file from EDI.',
-                                                               rescue = 'manual or one-off scripted extraction needed from a scanned document or non-regular table'
-                                                   )),
-                         data_source_download_URL = list(description = 'list of for data download, as ; seperated list',
-                                                         type = 'character'), #TODO formatting check here?
-                         data_source_filename = list(description = 'list of data file names that match the data downloads, as ; seperated list',
+                         data_source_citation = list(description = 'data citation',
                                                      type = 'character'),
-                         data_source_citation = list(description = 'suggested data citation, includes acknowledgements',
-                                                     type = 'character'),
-                         data_source_DOI = list(description = 'DOI for associated data object, might be different from download URL',
+                         data_source_DOI = list(description = 'DOI for entire associated data object',
                                                 type = 'character'),
                          data_source_abstract = list(description = 'Abstract for data citation',
                                                      type = 'character'),
                          data_source_license = list(description = 'data license and use-case restrictions',
                                                     type = 'chacacter'),
                          data_source_notes = list(description = 'Free notes field: why this data source was selected,list of data variables, keywords, and associated publications',
-                                                  type = 'character'))
+                                                  type = 'character'),
+                         data_source_access = list(description = 'how the data was accessed',
+                                                   type = list(regular_tables = 'download as individual data tables or as zipfiles with one header row and regular structure. Possible meta information in header defined as /* */ from Pangaea or as xml-style file from EDI.',
+                                                               rescue = 'manual or one-off scripted extraction needed from a scanned document or non-regular table'
+                                                   )),
+                         data_source_download = list(description = 'list of for data download including the name of the file paired with the download url',
+                                                     type = 'pair-list')
+                         )
   return(data_source.ls)
 }
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# CoSoDat
-Community soil data
+# Community soil data (CoSoDat)
+
+This project aims to develop a soil vocabulary to represent experimental research data.
+
+
+# Project layout
+
+Currently vocabularies are represented as a R list and designed to describe soil data in enough detail to be able to design a data harmonization project.
+
+# Current status
+
+We currently have vocabulary designed to describe soil data sources and bulk density with coarse fraction.
+
+Data sources are identified in `data/data_sources.tsv` with prioritization given (but not exclusive) to data reporting soil bulk density located in the global south.
+These data sources were used to check the bulk density description design and will be used to prototype a data integration and annotation workflow in the future.
+
+# Future plan
+
+We need to develop integration with a data harmonization workflow.
+
+Future vocabularies:
+  1) organic carbon fraction
+  2) soil sample location
+      - georeference (lat, long, datum)
+      - collection time (year, month, day, hour, minute, timezone)
+      - layer depth (top, bottom, zero-reference)
+      - physiochemical fractionation
+  3) sampling strategy and design
+  4) CO2/CH2 flux
+  5) nitrogen
+  6) phosphorus
+  7) case/control treatment
+    - lab incubation (temperature & moisture)
+    - field warming experiment
+    - rainout experiment
+  8) microbial biomass
+    - fumigation
+    - substrate enrichment
+  9) isotope quantification
+
+After we have tested the vocabularies across several data sets, we plan to spin off a soil ontology January 2022.
+
+# Direct outputs
+
+K Todd-Brown, P Areeveso, J Atkins, A Brown, J Loisel, M O'Brien, K Patel, D Sihi. Tools from data-centered community of practice in soils, AGU Fall Meeting, Dec 2021
