959b39885cdb41733c48fe1bfe8d79cd0908ab88
K Todd-Brown <ktoddbrown@ufl.edu>
2021-09-08 13:08:49 -0400

    added common soil pH range; and flagged common methods

--- R/make_soil_pH.R
+++ R/make_soil_pH.R
@@ -6,14 +6,14 @@
 #' @examples
 make_soil_pH <- function(){
   soil_pH.ls <- list(soil_pH = list(
-    value = list(description = 'soil pH measurement',
+    value = list(description = 'soil pH measurement; [common values 3.5, 11.5]',
                  range = list(min = 0, max = 14)),
     unit = '[unitless] - negative of the base 10 logarithm of the activity of the H+ ion',
     precision = list('0.5' = '0.5 unit of percision, common with general consumer litmus paper',
                      '0.1' = '0.1 unit of percision'),
     method = list(soil_water_status = list('ovan dry', 'air dry', 'field moist'),
-                  solute = list('water' = 'soil sample diluted with pure water',
-                                'CaCl2' = list(description = 'soil sample diluted with CaCl2 in water',
+                  solute = list('water' = 'soil sample diluted with pure water [common]',
+                                'CaCl2' = list(description = 'soil sample diluted with CaCl2 in water [common]',
                                                solute_molarity = list('1M', '0.1M')),
                                 'KCl'   = list(description = 'soil sample diluted with KCl in water',
                                                solute_molarity = list('1M', '0.1M')),
