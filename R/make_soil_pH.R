aca5b18c07e933049433de9ee26fa7c8bdc0013b
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2021-09-08 09:17:43 -0700

    Update make_soil_pH.R

--- R/make_soil_pH.R
+++ R/make_soil_pH.R
@@ -7,9 +7,9 @@
 make_soil_pH <- function(){
   soil_pH.ls <- list(soil_pH = list(
     value = list(description = 'soil pH measurement',
-                 range = list(min = 0, max = 10)),
+                 range = list(min = 0, max = 14)),
     unit = '[unitless] - negative of the base 10 logarithm of the activity of the H+ ion',
-    percision = list('0.5' = '0.5 unit of percision, common with general consumer litmus paper',
+    precision = list('0.5' = '0.5 unit of percision, common with general consumer litmus paper',
                      '0.1' = '0.1 unit of percision'),
     method = list(soil_water_status = list('ovan dry', 'air dry', 'field moist'),
                   solute = list('water' = 'soil sample diluted with pure water',
@@ -25,7 +25,7 @@ make_soil_pH <- function(){
                                     '1:2.5' = '1 to 2.5 soil to solute',
                                     '1:5' = '1 to 5 soil to solute',
                                     '1:10' = '1 to 10 soil to solute'),
-                  instrament = list(litmus = 'litmus paper',
+                  instrument = list(litmus = 'litmus paper',
                                     meter = 'pH meter')
     )
   ))
