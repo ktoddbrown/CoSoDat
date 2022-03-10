c421965d1d02cc7c0a43604a340151de572a6d6c
K Todd-Brown <ktoddbrown@gmail.com>
2022-03-10 11:27:22 -0500

    documentation and minor code cleaning for parsing function.

--- R/parse_graphviz.R
+++ R/parse_graphviz.R
@@ -1,30 +1,51 @@
-# Read in the lines of the file
-# remove everything outside of the parens
-# remove all comments
-# remove all empty lines
-# strip graph <name> {} out
-# sellect all lines without ->
-# split and rotate
-# select all lines with -> 
-# split split and rotate
-
-temp <- paste0(readLines('graphviz/loss_on_ignition.gv'), collapse = '\n')
-
-#remove opening brackets
-temp2 <- gsub('\\}[^\\}]*$', '', gsub('^.*graph [^\\{]+\\{', '', temp))
-
-#remove comments
-temp3 <- gsub('//[^\n]*\n', '', temp2)
-library(tidyverse)
-data.frame(lines = stringr::str_trim(unlist(strsplit(temp3, split = '\n')))) %>%
-  filter(grepl('\\S', lines)) %>%
-  mutate(edges = stringr::str_trim(gsub('\\[.*\\]', '', lines)),
-         attributes = stringr::str_trim(gsub('\\].*$', '', gsub('^.*\\[', '', lines)) )) %>%
-  tidyr::separate(col = edges, into = c('from', 'to'), sep = '->') %>%
-  tidyr::separate_rows(attributes, sep = ',') %>%
-  mutate(to = gsub('\\}', '', gsub('\\{', '', to))) %>%
-  mutate(across(everything(), stringr::str_trim)) %>%
-  separate_rows(to, sep = '[\\s;,]+') %>%
-  separate(col = attributes, into = c('type', 'value'), sep = ' ?= ?') %>% 
-  pivot_wider(names_from = 'type', values_from = 'value') %>%view
+#' Convert graph to table
+#'
+#' @param filename 
+#'
+#' @return
+#' @export
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_trim
+#' @importFrom tidyr separate separate_row pivot_wider
+#' @importFrom dplyr filter mutate 
+#'
+#' @examples
+parse_graphviz <- function(filename){
   
+  # read in the file and put it all in one string
+  fullString <- paste0(readLines(filename), collapse = '\n')
+  
+  graphName <- gsub('^(di)?graph +', '', gsub(' +\\{.*', '', fullString))
+  
+  #remove opening/closing brackets
+  bodyString <- gsub('\\}[^\\}]*$', '', gsub('^(di)?graph [^\\{]+\\{', '', fullString))
+  
+  #remove comments with leading //
+  noCommentBody <- gsub('//[^\n]*\n', '', bodyString)
+  
+  #Pull appart the string to generate master table of all nodes/edge properties
+  ans <- data.frame(lines = stringr::str_trim(unlist(strsplit(noCommentBody, split = '\n')))) %>% #split up the lines
+    #only take non-empty lines
+    dplyr::filter(grepl('\\S', lines)) %>%
+    #create the edges by removing everything within the properties defied by []
+    dplyr::mutate(edges = stringr::str_trim(gsub('\\[.*\\]', '', lines)),
+                  #create the attributes by removing everything inside []
+                  attributes = stringr::str_trim(gsub('\\].*$', '', gsub('^.*\\[', '', lines)) )) %>%
+    #split out the 'edge' into nodes based on the direction separator
+    tidyr::separate(col = edges, into = c('from', 'to'), sep = '->?', fill = 'right') %>%
+    #Split out the attributes into their own rows
+    tidyr::separate_rows(attributes, sep = ',') %>%
+    ####break out the edges that are assigned multiple targets by first
+    #strip out the {} from the end and start
+    dplyr::mutate(to = stringr::str_trim(gsub('\\}\\s*$', '', gsub('^\\s*\\{', '', to)))) %>%
+    #separate onto new rows the nodes based on spaces, ';' or ','
+    tidyr::separate_rows(to, sep = '[\\s;,]+')  %>%
+    #move the attributes into type/value pairs
+    tidyr::separate(col = attributes, into = c('type', 'value'), sep = ' ?= ?') %>%
+    #clean up spaces
+    dplyr::mutate(across(everything(), stringr::str_trim))%>%
+    #label the attributes in their own columns
+    tidyr::pivot_wider(names_from = 'type', values_from = 'value') 
+  
+  return(ans)
+}
